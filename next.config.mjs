81d8766d563e23c0cd7af01404e894647c62f762
ettoreveronese <everonese50@gmail.com>
2024-07-04 12:59:20 +0000

    v0.1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "production" ? "": undefined,
+  output: "export",
+  reactStrictMode: true,
+   experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
